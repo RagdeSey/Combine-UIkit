d22e6a08932143b7ffab3455eefbdc917b32d060
Emilio Vásquez <evasquez@applaudostudios.com>
2020-09-13 21:24:44 -0600

    🎉 Initial Commit
    - Add cocoapods to the project
    - Add CombineCocoa library
    - Add KingFisher
    - Improve directory order

---
+++ Combine-UIKit/Combine-UIKit/ViewControllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Combine-UIKit
+//
+//  Created by Emilio Vásquez on 9/13/20.
+//  Copyright © 2020 Emilio Vásquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
---
+++ Combine-UIKit/Pods/Target Support Files/Pods-Combine-UIKit/Pods-Combine-UIKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Combine_UIKit : NSObject
+@end
+@implementation PodsDummy_Pods_Combine_UIKit
+@end
